fa5fe95453946fb8fbac9067aed9e67fd71db07e
Your Name <you.example.com>
2016-05-18 10:53:55 -0400

    Submitting ConsoleApplication5MarchSevenFive

---
+++ ConsoleApplication5MarchSevenFive/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5MarchSevenFive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Number Here");
+            int n = int.Parse(Console.ReadLine());
+            int startingNum = n;
+
+            decimal factorial = 1;
+            do
+            
+            
+           
+            {
+                if (n <= 1)
+                {
+                    break;
+                }
+                factorial *= n;
+                n--;
+
+            }
+            while (true) ;
+
+                Console.WriteLine(startingNum + "! = " + factorial);
+
+
+        }
+    }
+}
